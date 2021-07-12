2ea06541bd0aaafd4606fd71dfa6c89c9a9ccf44
Isiah Jones <jones.isiah.blhs@gmail.com>
2021-07-12 14:32:50 -0400

    Initial commit

---
+++ AdressBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AdressBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdressBook.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
